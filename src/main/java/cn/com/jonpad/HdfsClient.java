669b336f06036a215ef415e7f1061c9de60966e3
JonChan <jonchan.hv@outlook.com>
2020-01-03 23:13:00 +0800

    文件上传测试

--- src/main/java/cn/com/jonpad/HdfsClient.java
+++ src/main/java/cn/com/jonpad/HdfsClient.java
@@ -16,23 +16,42 @@ import java.time.LocalDateTime;
  * @date 2019/12/30 00:08
  */
 public class HdfsClient {
-    @Test
-    public void testMkdirs() throws IOException, InterruptedException, URISyntaxException {
+  @Test
+  public void testMkdirs() throws IOException, InterruptedException, URISyntaxException {
 
-        // 1 获取文件系统
-        Configuration configuration = new Configuration();
-        // 配置在集群上运行 方案1 需要设置原型JVM变量 [ -DHADOOP_USER_NAME=hadoop ]
-        // configuration.set("fs.defaultFS", "hdfs://hadoop1:9000");
-        // FileSystem fs = FileSystem.get(configuration);
+    // 1 获取文件系统
+    Configuration configuration = new Configuration();
+    // 配置在集群上运行 方案1 需要设置原型JVM变量 [ -DHADOOP_USER_NAME=hadoop ]
+    // configuration.set("fs.defaultFS", "hdfs://hadoop1:9000");
+    // FileSystem fs = FileSystem.get(configuration);
 
-        //  配置在集群上运行 方案2
-        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop1:9000"), configuration, "hadoop");
+    //  配置在集群上运行 方案2
+    FileSystem fs = FileSystem.get(new URI("hdfs://hadoop1:9000"), configuration, "hadoop");
 
-        // 2 创建目录
-        fs.mkdirs(new Path("/test"+ Instant.now().toEpochMilli()));
+    // 2 创建目录
+    fs.mkdirs(new Path("/test" + Instant.now().toEpochMilli()));
 
-        // 3 关闭资源
-        fs.close();
-    }
+    // 3 关闭资源
+    fs.close();
+  }
+
+
+  /**
+   * 文件上传测试
+   */
+  @Test
+  public void testUploadFile() throws Exception {
+    // 1 获取FS对象
+    FileSystem fileSystem = FileSystem.get(new URI("hdfs://hadoop1:9000"), new Configuration(), "hadoop");
+
+    // 2 执行上传API
+    fileSystem.copyFromLocalFile(new Path("/Users/jonchan/Downloads/staff.sql"), new Path("/user/hadoop/staff.sql"));
+
+    // 3 关闭资源
+    fileSystem.close();
+  }
+
+
+
 
 }
