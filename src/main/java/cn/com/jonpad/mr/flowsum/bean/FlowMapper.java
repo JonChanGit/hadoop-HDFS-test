d00e962f8f496eff03d7c5e0c99c55d78ad6a3cf
JonChan <jonchan.hv@outlook.com>
2020-06-18 23:51:15 +0800

    序列化示例

---
+++ src/main/java/cn/com/jonpad/mr/flowsum/bean/FlowMapper.java
@@ -0,0 +1,31 @@
+package cn.com.jonpad.mr.flowsum.bean;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * @author Jon Chan
+ * @date 2020/6/18 21:37
+ */
+public class FlowMapper extends Mapper<LongWritable, Text, Text, FlowBean> {
+
+  Text k = new Text();
+  FlowBean v = new FlowBean();
+
+  @Override
+  protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    String[] items = value.toString().split("\t");
+
+    String phone = items[1];
+    k.set(phone);
+
+    v.setDownFlow(Long.parseLong(items[4]));
+    v.setUpFlow(Long.parseLong(items[5]));
+
+    context.write(k, v);
+
+  }
+}
---
+++ src/main/java/cn/com/jonpad/mr/flowsum/bean/FlowReduce.java
@@ -0,0 +1,32 @@
+package cn.com.jonpad.mr.flowsum.bean;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * @author Jon Chan
+ * @date 2020/6/18 23:41
+ */
+public class FlowReduce extends Reducer<Text, FlowBean, Text, FlowBean> {
+
+  FlowBean v = new FlowBean();
+
+  @Override
+  protected void reduce(Text key, Iterable<FlowBean> values, Context context) throws IOException, InterruptedException {
+
+    long sumUp = 0;
+    long sumDown = 0;
+
+    for (FlowBean value : values) {
+      sumUp += value.getUpFlow();
+      sumDown += value.getDownFlow();
+    }
+
+    v.set(sumUp, sumDown);
+
+    context.write(key, v);
+  }
+}
